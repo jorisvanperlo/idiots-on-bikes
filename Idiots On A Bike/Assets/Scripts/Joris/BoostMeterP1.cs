3ea291b944b17d5f686e70aed24e3b048d3684d1
StudioSkythorne <159907973+StudioSkythorne@users.noreply.github.com>
2024-06-28 14:37:01 +0200

    I tried :D

--- Idiots On A Bike/Assets/Scripts/Joris/BoostMeterP1.cs
+++ Idiots On A Bike/Assets/Scripts/Joris/BoostMeterP1.cs
@@ -7,8 +7,9 @@ public class BoostMeterP1 : MonoBehaviour
 {
     public GameObject p1;
     public Scrollbar scroll;
+
     void Update()
     {
-        scroll.value = p1.GetComponent<Movement>().boost / 20.0f;
+        scroll.size = p1.GetComponent<Movement>().boost / 20.0f;
     }
 }
---
+++ Idiots On A Bike/Assets/Scripts/Joris/BoostMeterP2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoostMeterP2 : MonoBehaviour
+{
+    public GameObject p2;
+    public Scrollbar scroll;
+    
+    void Update()
+    {
+        scroll.size = p2.GetComponent<MovementP2>().boost / 20.0f;
+    }
+}
